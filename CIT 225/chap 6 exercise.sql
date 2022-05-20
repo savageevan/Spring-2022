22842dd264cfa9082b862e647d17afdb8863a1c9
Evan Savage <esavage3@byui.edu>
2022-05-20 08:43:26 -0600

    m

--- CIT 225/chap 6 exercise.sql
+++ CIT 225/chap 6 exercise.sql
@@ -6,20 +6,72 @@ CREATE TABLE b (text varchar(1));
 INSERT INTO a (text) VALUES ("L"), ("M"), ("N"), ("O"), ("P");
 INSERT INTO B (text) VALUES ("P"), ("Q"), ("R"), ("S"), ("T");
 
-SELECT text from a
-EXCEPT
-SELECT text from b;
-
 use sakila;
 
+--- Question 2
 SELECT a.first_name, a.last_name
 FROM actor a
-WHERE a.last_name LIKE "L%"
-UNION ALL
+WHERE last_name LIKE "L%"
+UNION
 SELECT c.first_name, c.last_name
 FROM customer c
-WHERE c.last_name LIKE "L%";
+WHERE last_name LIKE "L%";
 
-SELECT ct.city_id AS id, ct.city as name
-FROM city ct;
+--- Question 3
+SELECT a.first_name, a.last_name
+FROM actor a
+WHERE last_name LIKE "L%"
+UNION
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE last_name LIKE "L%"
+ORDER BY last_name;
+
+--- Question 4
+SELECT c.city_id AS id, c.city AS name
+FROM city c
+WHERE c.city LIKE "S%o%a"
+UNION ALL
+SELECT co.country_id AS id, co.country AS name
+FROM country co
+WHERE co.country LIKE "S%o%a";
+
+--- Question 5
+SELECT c.city_id AS id, c.city AS name
+FROM city c
+WHERE c.city LIKE "S%o%a"
+UNION ALL
+SELECT co.country_id AS id, co.country AS name
+FROM country co
+WHERE co.country LIKE "S%o%a"
+ORDER BY name;
+ 
+ --- Question 6
+SELECT a.first_name, a.last_name, f.title
+FROM actor a INNER JOIN film_actor fa
+ON a.actor_id = fa.actor_id INNER JOIN film f
+ON fa.film_id = f.film_id
+WHERE a.first_name LIKE "M%" AND f.title LIKE "LOVE%"
+UNION
+SELECT a.first_name, a.last_name, f.title
+FROM actor a INNER JOIN film_actor fa
+ON a.actor_id = fa.actor_id INNER JOIN film f
+ON fa.film_id = f.film_id
+WHERE a.last_name LIKE "W%" AND f.title LIKE "LIFE%"
+ORDER BY last_name;
+
+--- Question 7
+SELECT a.last_name, f.title
+FROM actor a INNER JOIN film_actor fa
+ON a.actor_id = fa.actor_id INNER JOIN film f
+ON fa.film_id = f.film_id
+WHERE a.first_name LIKE "M%" AND f.title LIKE "LOVE%"
+UNION
+SELECT a.last_name, f.title
+FROM actor a INNER JOIN film_actor fa
+ON a.actor_id = fa.actor_id INNER JOIN film f
+ON fa.film_id = f.film_id
+WHERE a.last_name LIKE "W%" AND f.title LIKE "LIFE%"
+ORDER BY last_name;
+
 
