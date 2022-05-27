64c1dcace393fda92af840cf6aaa87eefb8e8df9
Evan Savage <esavage3@byui.edu>
2022-05-27 09:49:49 -0600

    ye

--- CIT 225/chap 7 exercise.sql
+++ CIT 225/chap 7 exercise.sql
@@ -1,5 +1,19 @@
---- Exercise 7
-Set @string := '2024-02-29';
+--- Exercise 1
+SELECT substring("Please find the substring in this string",17,9) AS "parsed";
+
+--- Exercise 2
+SELECT abs(-25.76823) as "abs", sign(-25.76823) as "sign", round(-25.76823, 2) as "round";
+
+--- Exercise 3
+SELECT extract(MONTH FROM CURRENT_DATE()) as "month";
+
+--- Exercise 4
+SET @string := 'Sorcer''s Stone';
+SELECT @string;
+SELECT replace(@string, "er", "erer") as "title";
+
+--- Exercise 5
+SET @string := '2024-02-29';
 SELECT DATE(@string) AS "date";
 SELECT date_format(DATE(@string),'%d-%b-%y') AS "date"; 
 
